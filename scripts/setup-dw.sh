861755b48774147e9b5e1368657b96ddcf39ce83
Mark Smith <mark@dreamwidth.org>
2013-07-26 02:38:50 +0000

    Checkpoint commit

--- scripts/setup-dw.sh
+++ scripts/setup-dw.sh
@@ -3,6 +3,9 @@
 # Designed to be run as part of the Docker setup.
 #
 
+# Copies over our configuration directory.
+rsync -avPW $LJHOME/ext/dw-docker/config/ $LJHOME/ext/local
+
 # Database setup and configuration.
 echo "CREATE DATABASE dw_schwartz" | mysql -uroot
 cat $LJHOME/ext/dw-docker/files/theschwartz-schema.sql | mysql -uroot dw_schwartz
@@ -12,9 +15,6 @@ $LJHOME/bin/upgrading/update-db.pl -r
 $LJHOME/bin/upgrading/update-db.pl -r
 $LJHOME/bin/upgrading/update-db.pl -r -p
 $LJHOME/bin/upgrading/texttool.pl load
-
-# Copies over our configuration directory.
-rsync -avPW dw-docker/config/ local
 
 # Now replace some system configuration directories.
 rm -rf /etc/apache2
