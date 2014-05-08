25b32e8d41f5a1145c0df668dd95e4ff2538c5fb
Afuna <fu@dreamwidth.org>
2014-05-08 16:25:18 -0700

    Create a one-time setup script
    -- /opt/init-db.sh should set up the linked mysql data container, but
    doesn't need to be run more than once
    
    -- remove the now unnecessary startup script, and just use mysqld as the
    entry point

--- mysql/scripts/init-db.sh
+++ mysql/scripts/init-db.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# we need to run the setup at startup instead of when building the image
-# because it's only at startup that we're linked to a data container
-
 # setup database
 mysql_install_db
 
@@ -10,6 +7,3 @@ mysql_install_db
 /usr/bin/mysqld_safe &\
     sleep 3s &&\
     echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-
-# now start mysqld
-/usr/sbin/mysqld
