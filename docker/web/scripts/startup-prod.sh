ec48dd181e1c88789ee14868949f3e1661bbbb3f
Mark Smith <mark@qq.is>
2019-12-01 08:19:58 +0000

    Easier debugging

--- docker/web/scripts/startup-prod.sh
+++ docker/web/scripts/startup-prod.sh
@@ -2,8 +2,9 @@
 
 set -xe
 
-# Validate that the system is set up and working correctly.
-perl -I$LJHOME/extlib/ $LJHOME/bin/checkconfig.pl
+# Validate that the system is set up and working correctly. If this fails, sleep forever
+# so that someone can log in and debug.
+perl -I$LJHOME/extlib/ $LJHOME/bin/checkconfig.pl || sleep infinity
 
 # Kick off Apache
 cp $LJHOME/ext/local/dreamwidth-prod.conf $LJHOME/ext/local/etc/apache2/sites-enabled/dreamwidth.conf
